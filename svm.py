afdddcc32a42441a6df09fbeb3cea0f54c06eae8
ruic3429 <ruic3429@gmail.com>
2017-11-06 16:23:46 -0500

    init

---
+++ svm.py
@@ -0,0 +1,75 @@
+import numpy as np
+import numpy.linalg as la
+import cvxopt.solvers
+
+MIN_SUPPORT_VECTOR_MULTIPLIER = 1e-5
+
+class SVMTrainer(object):
+    def __init__(self, k, c):
+        self._k = k
+        self._c = c
+
+    def trainer(self, x, y):
+        lagrange_multipliers = self._compute_multipliers(x, y)
+        return self._construct_predictor(x, y, lagrange_multipliers)
+
+    def _gram_matrix(self, x):
+        n_samples, n_features = x.shape
+        mat = np.zeros((n_samples))
+        for i, x_i in enumerate(X):
+            for j, x_j in enumerate(X):
+                mat[i, j] = self._kernel(x_i, x_j)
+        return mat
+
+    def _construct_predictor(self, x, y, lagrange_multipliers):
+        n_samples, n_features = x.shape
+        support_vector_indices = \
+            lagrange_multipliers > MIN_SUPPORT_VECTOR_MULTIPLIER
+
+        support_vector_labels = y[support_vector_indices]
+        support_multipliers = lagrange_multipliers[support_vector_indices]
+        support_vectors = X[support_vector_indices]
+
+
+        bias = np.mean(
+            [y_k - SVMPredictor(
+                kernel=self._k,
+                bias= 0,
+                weights=support_multipliers,
+                support_vectors=support_vectors,
+                support_vector_labels=support_vector_labels).predict(x_k)
+             for (y_k, x_k) in zip(support_vector_labels, support_vectors)])
+
+        return SVMPredictor(
+            kernel=self._k,
+            bias=bias,
+            weights=support_multipliers,
+            support_vectors=support_vectors,
+            support_vector_labels=support_vector_labels)
+
+    def _compute_multipliers(self, x, y):
+        n_samples, n_features = X.shape
+
+        mat = self._gram_matrix(x)
+
+        P = cvxopt.matrix(np.outer(y, y) * mat)
+        q = cvxopt.matrix(-1 * np.ones(n_samples))
+
+        G_std = cvxopt.matrix(np.diag(np.ones(n_samples) * -1))
+        h_std = cvxopt.matrix(np.zeros(n_samples))
+
+        G_slack = cvxopt.matrix(np.diag(np.ones(n_samples)))
+        h_slack = cvxopt.matrix(np.ones(n_samples) * self._c)
+
+        G = cvxopt.matrix(np.vstack((G_std, G_slack)))
+        h = cvxopt.matrix(np.vstack((h_std, h_slack)))
+
+        A = cvxopt.matrix(y, (1, n_samples))
+        b = cvxopt.matrix(0.0)
+
+        solution = cvxopt.solvers.qp(P, q, G, h, A, b)
+
+        return np.ravel(solution['x'])
+
+class SVMPredictor(object)
+#你们写一下predictor
