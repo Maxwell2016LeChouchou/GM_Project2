ed2ed17e882743e2c0d35d183f733da7a4586c8c
zhaohuixxt <31806195+zhaohuixxt@users.noreply.github.com>
2017-11-12 12:18:46 -0500

    Update logclassifier.m

--- logclassifier.m
+++ logclassifier.m
@@ -3,24 +3,30 @@ clc;
 close all;
 %% load data and preprocession
 load('classify_d5_k3_saved1.mat');
-x=[class_1,class_2];
+inputx=[class_1,class_2];
 n_one=size(class_1,2);
 n_zero=size(class_2,2);
-[nFeatures,nSamples]=size(x);
-y=[ones(1,n_one),zeros(1,n_zero)];
+[nFeatures,nSamples]=size(inputx);
+inputy=[ones(1,n_one),zeros(1,n_zero)];
+% split data into test samples and training samples
+[x xTest y yTest] = splitData(inputx', inputy');
+clear inputx, inputy;
+
+%initialize variables 
 weight=ones(nFeatures,1);
 bias=1;
-nIter=100000;
-alpha=0.02;
+nIter=10000;
+alpha=0.001;
 threthold=0.05;
-%% 
+
+%% training process
 for i=1:nIter
 %% forward propagation
 h = weight'*x+bias;        
 % linear transform
 g= 1.0 ./ (1.0 + exp(-h)); 
 % activation function
-error=[y(:,1:n_one).*log(g(:,1:n_one)),(1-y(:,n_one+1:n_one+n_zero)).*log(1-g(:,n_one+1:n_one+n_zero))];
+error=[y(:,find(y==1)).*log(g(:,find(y==1))),(1-y(:,find(y==0))).*log(1-g(:,find(y==0)))];
 J=(-1/nSamples)*sum(error);
 % loss function
 if J<threthold
@@ -34,6 +40,10 @@ weight=weight-alpha*x*deltah';
 bias=bias-alpha*sum(deltah);
 % update bias
 end
+
+%% classifier process
+
+
 
 
 
